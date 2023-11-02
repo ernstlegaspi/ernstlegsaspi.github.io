715979e288f8d084388392ae4859cdcef67be2aa
ernstlegaspi <legaspiernstdanielle@gmail.com>
2023-11-03 00:08:03 +0800

    Updates

---
+++ static/js/485.d6ccd398.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[485],{485:function(n,r,e){e.r(r),e.d(r,{default:function(){return c}});e(791);var i=e(820),t=e(184),s=function(n){var r=n.children;return(0,t.jsx)("div",{className:"inset",children:(0,t.jsx)("div",{className:"white-bg",children:r})})},c=function(n){var r=n.showTwitterClone;return(0,t.jsx)(s,{children:(0,t.jsx)("div",{className:"ml-2 mt-2 bg-red-500 cursor-pointer",onClick:function(){return r(!1)},children:(0,t.jsx)(i.oHP,{})})})}}}]);
+//# sourceMappingURL=485.d6ccd398.chunk.js.map
